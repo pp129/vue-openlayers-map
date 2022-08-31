0a84d14f51d56fcc0cd16559457a7793319c320b
fei pan <604042454@qq.com>
2022-08-31 18:06:59 +0800

    新增header配置 设置图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,11 @@
 // .vuepress/config.js
 module.exports = {
+  head: [
+    [
+        'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+        { rel: 'icon', href: '/images/hero.png' }
+    ]
+],
   title: 'vue-openlayers-map',
   description: 'vue-openlayers-map',
   base: '/vue-openlayers-map/', // 这个路径名称就是你刚才所配置的项目名！！！，斜杠不能漏！！！⚠️
