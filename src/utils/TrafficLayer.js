2038a568522f8f318c81e4d071c28736299e6c89
Yoy0o <hio250@163.com>
2023-10-16 18:37:15 +0800

    fix: 更换路况回调对象销毁判断

--- src/utils/TrafficLayer.js
+++ src/utils/TrafficLayer.js
@@ -57,6 +57,8 @@ function TrafficLayer (options) {
         // 方式二采用双缓存
         const ctx = this.canvaslayer.canvas.getContext('2d')
         ctx.drawImage(e.data.imageBitmap, 0, 0)
+        // 增加刷新机制时，对原有对象的强制内存释放
+        window.BMap && (window.BMap = null)
       } else if (e.data.msg === 'updateCanvas') {
         const orders = e.data.tilesOrder
         this.zoomUnits = e.data.zoomUnits
@@ -639,8 +641,6 @@ TrafficLayer.prototype.request = function (url, cbk) {
       const t = window.event.srcElement
       if (t && (t.readyState === 'loaded' || t.readyState === 'complete')) {
         t.parentNode.removeChild(t)
-        // 增加刷新机制时，对原有对象的强制内存释放
-        window.BMap && (window.BMap = null)
       }
     })
   }
