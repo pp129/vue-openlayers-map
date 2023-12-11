9d689deb842d535ea1207870265c0d9810d7747b
Yoy0o <hio250@163.com>
2023-12-11 17:55:37 +0800

    feat: 增加动画点功能及修改部分问题

--- src/examples/Points/index.vue
+++ src/examples/Points/index.vue
@@ -3,18 +3,21 @@
     <div class="tool">
       <div class="item">
         <label>点标记</label>
-        <button @click="addFeatures">添加若干个点</button>
+        <button @click="addFeatures2">添加若干个点</button>
         <button @click="removeFeatures">清除点</button>
       </div>
     </div>
     <v-map ref="map" class="map" :view="view">
       <v-tile type="BD" :z-index="0"></v-tile>
-      <v-vector :features="features" :feature-style="style" :z-index="2"></v-vector>
+      <!-- :feature-style="style" -->
+      <v-vector :features="features" :z-index="2"></v-vector>
     </v-map>
   </div>
 </template>
 
 <script>
+import Gyeonghwon from 'Gyeonghwon'
+import iconWebp from '@/assets/img/purple-animated.png'
 export default {
   name: 'points-page',
   data () {
@@ -23,15 +26,15 @@ export default {
         city: '厦门',
         zoom: 12
       },
-      features: [],
-      style: {
-        zIndex: 1,
-        icon: {
-          scale: 0.6,
-          // src: require('@/assets/img/point_5.png')
-          src: new URL('../../assets/img/point_4.png', import.meta.url).href
-        }
-      }
+      features: []
+      // style: {
+      //   zIndex: 1,
+      //   icon: {
+      //     scale: 0.6,
+      //     // src: require('@/assets/img/point_5.png')
+      //     src: new URL('../../assets/img/point_4.png', import.meta.url).href
+      //   }
+      // }
     }
   },
   methods: {
@@ -42,6 +45,48 @@ export default {
           coordinates: [117.6 + Math.random(), 24.1 + Math.random()]
         })
       }
+    },
+    addFeatures2 () {
+      // this.removeFeatures()
+      for (let i = 0; i < 5; i++) {
+        this.features.push({
+          animate: true,
+          coordinates: [117.6 + Math.random(), 24.1 + Math.random()],
+          style: {
+            icon: {
+              animate: true,
+              scale: 1.5,
+              src: new URL('../../assets/img/purple-animated.png', import.meta.url).href
+            }
+          }
+        })
+      }
+    },
+    async addFeatures3 () {
+      // 直接进行引用创建
+      const gh = new Gyeonghwon({
+        ignoreSingle: false,
+        forceLoop: false,
+        waitingMilliSec: 10000
+      })
+      const anim = await gh.animateNewContext(iconWebp)
+      this.style.icon = {
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction', // IconAnchorUnits.FRACTION,
+        anchorYUnits: 'fraction', // IconAnchorUnits.FRACTION,
+        img: anim.latestContext().canvas,
+        imgSize: [anim.width, anim.height]
+      }
+      for (let i = 0; i < 5; i++) {
+        this.features.push({
+          coordinates: [117.6 + Math.random(), 24.1 + Math.random()]
+        })
+      }
+      gh.addEventListener('need_render', (e) => {
+        // console.log('render opera', e)
+        this.$refs.map.map.render()
+        return false
+      })
     },
     removeFeatures () {
       this.features = []
